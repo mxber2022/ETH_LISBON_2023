e60b51b2d139cb362fd772a57156074807f70e7c
mxber2022 <mxber2022@gmail.com>
2023-11-04 20:27:56 +0000

    web3mail

--- frontend/src/components/Iexec/Iexec.js
+++ frontend/src/components/Iexec/Iexec.js
@@ -1,10 +1,21 @@
 import './Iexec.css';
+import { IExecWeb3mail } from "@iexec/web3mail";
 
 function Iexec () {
 
+    const web3Provider = window.ethereum;
+    const web3mail = new IExecWeb3mail(web3Provider);
+
+    async function fetchMyMail() {
+        const listContact = await web3mail.fetchMyContacts()
+
+        console.log(listContact);
+    }
+
     return(
         <>
-            <h2>IExec Functions</h2>
+            <h2>IExec - Web3Mail</h2>
+            <button onClick={fetchMyMail}>Fetch All Mails</button>
         </>
     )
 }
